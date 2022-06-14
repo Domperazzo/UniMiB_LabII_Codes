0e909b12ec505749daeb3eac6323d4ce0f2e008a
Domperazzo <f.venturoli@campus.unimib.it>
2022-06-14 17:18:15 +0200

    fit RC circuit

---
+++ Circuiti3/Fit Vari/main.cpp
@@ -0,0 +1,115 @@
+/*
+	c++ -o main main.cpp `root-config --glibs --cflags`
+*/
+
+#include <stdlib.h>
+#include <cmath>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "TGraph.h"
+#include "TApplication.h"
+#include "TMultiGraph.h"
+#include "TCanvas.h"
+#include "TF1.h"
+#include "TFitResultPtr.h"
+#include "TFitResult.h"
+#include "TGraphErrors.h"
+#include "TStyle.h"
+#include "TH1F.h"
+#include "TMath.h"
+#include "TLatex.h"
+#include "TMultiGraph.h"
+#include "TAttFill.h"
+#include "TVirtualPad.h"
+
+
+int main(int argc, char **argv) {
+
+  TApplication theApp("theApp", &argc, argv);
+  gStyle->SetOptFit(1112);
+
+  std::ifstream dati;
+  dati.open("RC_h.txt", std::ios::in);
+  std::vector<double> v_y1, v_y2, v_x;
+  while (true) {
+      double y1, y2, x ;
+      dati >> y1 >> y2 >> x;
+      if(dati.eof()==true) break;
+      v_y1.push_back(y1);
+      v_y2.push_back(y2);
+      v_x.push_back(x);
+  }
+	dati.close();
+
+  std::ifstream errori;
+  errori.open("scarti.txt", std::ios::in);
+  std::vector<double> v_err;
+  while (true) {
+      double err;
+      errori >> err;
+      if(errori.eof()==true) break;
+      v_err.push_back(err);
+  }
+  errori.close();
+
+
+//Grafico e fit con y1
+
+TGraphErrors grafico;
+
+for (int i = 0; i < v_x.size(); i++) {
+  grafico.SetPoint(i, v_x.at(i), v_y1.at(i));
+  grafico.SetPointError(i, 0., 0.03064);
+}
+
+TF1 modello("modello", "(10.1*x*[0])/sqrt(1+x^2*[0]^2*102.01)");
+modello.SetParName(0, "C");
+
+TFitResultPtr fit_result = grafico.Fit(&modello, "S");
+
+
+grafico.SetMarkerStyle(20);
+grafico.SetMarkerSize(0.5);
+
+grafico.SetTitle("Funzione di trasferimento circuito RC");
+grafico.GetHistogram()-> GetXaxis()->SetTitle("Pulsazione #omega");
+grafico.GetHistogram()-> GetYaxis()->SetTitle("|H(#omega)|");
+
+
+//Grafico e fit con y2
+
+TGraphErrors grafico2;
+
+for (int i = 0; i < v_x.size(); i++) {
+  grafico2.SetPoint(i, v_x.at(i), v_y2.at(i));
+  grafico2.SetPointError(i, 0., 0.03064);
+}
+
+TF1 modello2("modello2", "1/(sqrt(1+x^2*[0]^2*102.01))");
+modello2.SetParName(0, "C");
+
+
+TFitResultPtr fit_result2 = grafico2.Fit(&modello2, "S");
+
+
+grafico2.SetMarkerStyle(20);
+grafico2.SetMarkerSize(0.5);
+
+grafico2.SetTitle("Funzione di trasferimento circuito RC ");
+grafico2.GetHistogram()-> GetXaxis()->SetTitle("Pulsazione #omega");
+grafico2.GetHistogram()-> GetYaxis()->SetTitle("|H(#omega)|");
+
+
+
+
+
+TCanvas c1;
+grafico2.Draw("AP");
+theApp.Run();
+
+
+
+
+  return 0;
+}
--- Circuiti3/RC_dati/calcola_dati.cpp
+++ Circuiti3/RC_dati/calcola_dati.cpp
@@ -23,9 +23,9 @@
 #include "TAttFill.h"
 #include "TVirtualPad.h"
 
-double massimo(std::vector<double> v_campione){
+double massimo(std::vector<double> v_campione, int range){
   double max = v_campione.at(0);
-  for (int i = 0; i < v_campione.size(); i++) {
+  for (int i = 0; i < range; i++) {
     if (v_campione.at(i) > max) {
       max = v_campione.at(i);
     }
@@ -39,6 +39,7 @@ int posizione_max(double massimo, std::vector<double> v_dati){
         return i;
       }
   }
+  return 0;
 }
 
 
@@ -49,7 +50,7 @@ int main(int argc, char **argv) {
 
 // Apertura CH1
 std::ifstream dati1;
-dati1.open("RC100_ch1.txt", std::ios::in);
+dati1.open("RC_45hz_CH1.txt", std::ios::in);
 std::vector<double> v_secondi1, v_corrente1;
 while (true) {
     double secondi1, corrente1;
@@ -62,7 +63,7 @@ dati1.close();
 
 // Apertura CH2
 std::ifstream dati2;
-dati2.open("RC100_ch2.txt", std::ios::in);
+dati2.open("RC_45hz_CH2.txt", std::ios::in);
 std::vector<double> v_secondi2, v_corrente2;
 while (true) {
     double secondi2, corrente2;
@@ -75,7 +76,7 @@ dati2.close();
 
 // Apertura MATH
 std::ifstream dati_math;
-dati_math.open("RC100_math.txt", std::ios::in);
+dati_math.open("RC_45hz_MATH.txt", std::ios::in);
 std::vector<double> v_secondi_math, v_corrente_math;
 while (true) {
     double secondi_math, corrente_math;
@@ -86,21 +87,46 @@ while (true) {
 }
 dati_math.close();
 
-  double ampiezza_massima_corrente_math = massimo(v_corrente_math);
+
+
+  double ampiezza_massima_corrente_math = massimo(v_corrente_math, v_secondi_math.size());
   int posizione_massimo_corrente_math = posizione_max(ampiezza_massima_corrente_math, v_corrente_math);
 
-  double ampiezza_massima_corrente1 = massimo(v_corrente1);
+  double ampiezza_massima_corrente1 = massimo(v_corrente1, v_secondi1.size());
   int posizione_massimo_corrente1 = posizione_max(ampiezza_massima_corrente1, v_corrente1);
 
-  double ampiezza_massima_corrente2 = massimo(v_corrente2);
+  double ampiezza_massima_corrente2 = massimo(v_corrente2, v_secondi2.size());
   int posizione_massimo_corrente2 = posizione_max(ampiezza_massima_corrente2, v_corrente2);
 
   double differenza_fase_primata = v_secondi1.at(posizione_massimo_corrente_math)-v_secondi2.at(posizione_massimo_corrente1);
 
   double differenza_fase_doppioprimata = v_secondi1.at(posizione_massimo_corrente1)-v_secondi2.at(posizione_massimo_corrente2);
 
+  std::cout << "delta phi primo: \t" << differenza_fase_primata << '\n';
+  std::cout << "delta phi secondo: \t" << differenza_fase_doppioprimata << '\n';
 
 
+TGraph grafico1, grafico2, grafico3;
+for (int i = 0; i < v_secondi2.size(); i++) {
+  grafico2.SetPoint(i, v_secondi2.at(i), v_corrente2.at(i));
+}
+
+for (int i = 0; i < v_secondi1.size(); i++) {
+  grafico1.SetPoint(i, v_secondi1.at(i), v_corrente1.at(i));
+}
+
+for (int i = 0; i < v_secondi_math.size(); i++) {
+  grafico3.SetPoint(i, v_secondi_math.at(i), v_corrente_math.at(i));
+}
+
+TMultiGraph multi;
+multi.Add(&grafico1);
+multi.Add(&grafico2);
+multi.Add(&grafico3);
+
+TCanvas c1;
+multi.Draw("AP");
+theApp.Run();
 
 
   return 0;
